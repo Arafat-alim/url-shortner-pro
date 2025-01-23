f20fa750d63420f4f3b339902ac46a8bca6ff697
arafat-alim <arafat97alim@gmail.com>
2025-01-24 01:05:20 +0530

    🔗 Mongo DB connection configured successfully.

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 //! load environment variables
 require("dotenv").config();
+const { default: helmet } = require("helmet");
 
 const express = require("express");
 const app = express();
 const cors = require("cors");
 
+//! mongodb connection
+const connectDB = require("./config/db.js");
+connectDB();
+
 //! Middlewares
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //! Test Routes
 app.get("/", (req, res) => {

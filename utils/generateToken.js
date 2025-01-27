c6f0131602c898be1b13765af3878db380d9d1bb
arafat-alim <arafat97alim@gmail.com>
2025-01-27 18:19:13 +0530

    ⬆️ File renamed: `generateToken` to `generateJwtToken`.

--- utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const Jwt = require("jsonwebtoken");
-
-const generateToken = (id) => {
-  return Jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "24h",
-  });
-};
-
-module.exports = { generateToken };
